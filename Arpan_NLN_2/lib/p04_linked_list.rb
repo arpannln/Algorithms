647b884852c88f797d71d8c26b89da129eb24231
Arpan NLN <arpannln@ucla.edu>
2018-02-27 21:03:36 -0800

    hash implementation

---
+++ Arpan_NLN_2/lib/p04_linked_list.rb
@@ -0,0 +1,69 @@
+class Node
+  attr_accessor :key, :val, :next, :prev
+
+  def initialize(key = nil, val = nil)
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@key}: #{@val}"
+  end
+
+  def remove
+    @prev.next = @next if @prev && @next
+    @next.prev = @prev if @prev && @next
+  end
+end
+
+class LinkedList
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @head.prev = @tail
+    @tail.next = @head
+    @tail.prev = @head
+  end
+
+  def [](i)
+    each_with_index { |node, j| return node if i == j }
+    nil
+  end
+
+  def first
+    @head.next
+  end
+
+  def last
+    @tail.prev
+  end
+
+  def empty?
+  end
+
+  def get(key)
+  end
+
+  def include?(key)
+  end
+
+  def append(key, val)
+  end
+
+  def update(key, val)
+  end
+
+  def remove(key)
+  end
+
+  def each
+  end
+
+  # uncomment when you have `each` working and `Enumerable` included
+  # def to_s
+  #   inject([]) { |acc, node| acc << "[#{node.key}, #{node.val}]" }.join(", ")
+  # end
+end
