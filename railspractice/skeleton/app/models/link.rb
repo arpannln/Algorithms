9e54ce56aba8062901876e599c7e9487394ab9af
Arpan NLN <arpannln@ucla.edu>
2018-04-02 16:04:43 -0700

    link validations

--- railspractice/skeleton/app/models/link.rb
+++ railspractice/skeleton/app/models/link.rb
@@ -1,2 +1,14 @@
 class Link < ApplicationRecord
+  validates :title, :url, presence: true
+
+  belongs_to :user,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: 'User'
+
+  has_many :comments,
+  primary_key: :id,
+  foreign_key: :link_id,
+  class_name: 'Comment'
+
 end
--- railspractice/skeleton/app/models/user.rb
+++ railspractice/skeleton/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :password_digest, :session_token, :username, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
-  before_validation :ensure_session_token
+  after_initialize :ensure_session_token
 
   attr_reader :password
 
@@ -16,8 +16,29 @@ class User < ApplicationRecord
   foreign_key: :user_id,
   class_name: 'Comment'
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user && user.valid_password?(password)
+    user
+  end
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
   def ensure_session_token
-    self.password_digest = SecureRandom.
+    self.session_token ||= SecureRandom.urlsafe_base64
   end
 
 end
