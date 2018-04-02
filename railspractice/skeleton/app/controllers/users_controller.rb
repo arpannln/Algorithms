1c76c6676a711e8546937fc2f99e54ebe4b8ff1f
Arpan NLN <arpannln@ucla.edu>
2018-04-02 16:17:30 -0700

    control her

---
+++ railspractice/skeleton/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+end
