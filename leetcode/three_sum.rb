acfb0c47c8e896111222301f55af2c1fc3bb7b3f
Arpan NLN <arpannln@ucla.edu>
2018-04-04 13:13:10 -0700

    keep at it

--- leetcode/three_sum.rb
+++ leetcode/three_sum.rb
@@ -1,1 +1,17 @@
 #use 2 sum on each of the elements for 3 sum
+
+def two_sum(nums, target)
+    future = {}
+
+    nums.each_with_index do |num, idx|
+        if (future[num])
+            return [future[num], idx]
+        end
+        future[target - num] = idx
+    end
+
+end
+
+def three_sum(nums, target)
+
+end
