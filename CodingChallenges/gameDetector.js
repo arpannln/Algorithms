7ab95a913fa9395fde262f9dd7d05f065839c67f
Arpan NLN <arpannln@ucla.edu>
2018-04-07 21:10:31 -0700

    first implelemnation

--- CodingChallenges/gameDetector.js
+++ CodingChallenges/gameDetector.js
@@ -1,6 +1,6 @@
 //two inputs
 //first input - large hash mapping strings to list of possible n-grams used to refer to the game
-//eg
+//e.g.
         // {
         // "CallOfDutyWW2": ["Call of duty world war two", "COD WW2", "COD WWII","WW2COD"],
         // "Fortnite": ["Fortnite", "Fort Nite"],
@@ -9,7 +9,7 @@
         // "WorldOfWarcraft": ["WoW the game", "world of warcraft"],
         // }
 //second input - large array of sentences
-//eg
+//e.g.
         //   ["I liked the last Destiny game, now I play Fortnite",
         // "Lol, no comment about that",
         // ...,
@@ -21,5 +21,36 @@
         // "I'm still playing TAG{WorldOfWarcraft,world of warcraft} since ww2"]
 
 
-// “CallOfDutyWW2”, must be represented in the final text as TAG{GameID,original
+// Note: “CallOfDutyWW2”, must be represented in the final text as TAG{GameID,original
 // text}, e.g. TAG{CallOfDutyWW2,Call of Duty world war two}.
+
+// Problem-simplified: need to parse through all the strings in the array and "discover"
+// any n-grams (does letter case count?) and then replace the n-gram with the respective gameID as well as the
+// trigger n-gram.
+
+// Initial-solution-plan:
+//    Create a reverse hash from the n-grams which would point to gameId
+//      e.g. {"Call of Duty world war two": "CallOfDutyWW2", }
+//    Iterate through sentences, locating any possible n-gram (this sounds quite expensive)
+//
+// Super naive approach:
+
+  function gameDetector (grams, sentences) {
+    //create our reverse hash
+    var reverse = {};
+    for (let key in grams) {
+      grams[key].forEach( (gram) => reverse[gram] = key)
+    }
+
+    sentences.forEach ( (sentence) =>
+      {
+        sentenc
+      }
+
+    )
+
+  }
+
+  function taggify (gameID, ngram) {
+    return `TAG{${gameID},${ngram}}`;
+  }
