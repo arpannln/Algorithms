97c55020a6828c4b6fa63f00947992509227916e
Arpan NLN <arpannln@ucla.edu>
2018-04-10 11:04:56 -0700

    one solution

--- CodingChallenges/gameDetector.js
+++ CodingChallenges/gameDetector.js
@@ -32,81 +32,10 @@
 //    Create a reverse hash from the n-grams which would point to gameId
 //      e.g. {"Call of Duty world war two": "CallOfDutyWW2", }
 //    Iterate through sentences, locating any possible n-gram (this sounds quite expensive)
-//
+// Gameplan pivot:
+//    search through entire string for each ngram, swap this ngram with a unique code so we dont find it again
+//    go through our unique codes and replace them with the associated ngram
 // Approach:
-
-
-  function gameDetectorOriginal (grams, sentences) {
-    // make our reverse hash for quick lookup
-    var gameID1 = ngramToGameID(grams);
-    var result = [];
-    // go through our sentences and split them into words and check all the different combinations
-    // for our n-grams
-    sentences.forEach ( (sentence) =>
-      {
-        var words = sentence.split(" ");
-
-        for (let i = 0; i < words.length; i++ ) {
-          var phrase = words[i];
-
-          for (let j = i + 1; j < words.length+1; j++) {
-            //punctuation checker
-            var punctuationCheck = removePunctuation(phrase);
-            var withoutPunctuation = punctuationCheck[0]; // our phrase without punctuation
-            var removed = punctuationCheck[1]; // our punctuation we need to append
-
-            if (gameID1[withoutPunctuation]) { //if we find  ngram
-              //use splice to replace the words in the n-gram with our tagged version so we avoid rechecks
-              words.splice(i, j - i, taggify(gameID1[withoutPunctuation], withoutPunctuation) + removed);
-            }
-
-            phrase += " " + words[j];
-          }
-        }
-        result.push(words.join(" "));
-      }
-
-    );
-    return result;
-  }
-  // function that hashes our ngrams to our gameIDs to improve efficiency of lookup cost
-  function ngramToGameID (grams) {
-    let gameID = {};
-    for (let key in grams) {
-      grams[key].forEach( (gram) => gameID[gram] = key);
-    }
-    return gameID;
-  }
-
-  //function to convert our ngram and gameID to proper notation
-  function taggify (gameID, ngram) {
-    return `TAG{${gameID},${ngram}}`;
-  }
-
-  //function to remove punctuation before we check to see if the phrase is in our hash
-  //was refactored to handle consecutive punctuation
-  //send back punctuation so we can append to our sentence after
-  const PUNCTUATION = ".,:;'?!".split("");
-
-  function removePunctuation(phrase) {
-    var punctuation = "";
-    while (PUNCTUATION.indexOf(phrase.slice(-1)) !== -1) {
-      punctuation = phrase.slice(-1) + punctuation;
-      phrase = phrase.slice(0, phrase.length-1);
-    }
-
-    return [phrase, punctuation];
-  }
-
-  //Time complexity discussion:
-  // making our new hash  will be largely overshadowed by the following operations
-  // going through each sentence is an operation of n
-    // within this operation we run through every combination of sequential words adding n^2 operations
-    // for every sentence
-        //within these nested loops we have two operations that have a worst case of O(n) - removePunctuation and splice
-  // this brings our time complexity to O(n^4), with a space (there is a large amount of unrecognized saved time because we traverse words rather than characters)
-  // However, a complexity of O(n^4) leads me to believe this is not the most optimal solution :(
-  // sooo, let's try again
 
 function gameDetector(grams, sentences) {
     // make our reverse hash for quick lookup
@@ -122,7 +51,7 @@ function gameDetector(grams, sentences) {
     // combine all our sentences so we eliminate one loop
     // unique join argument so we can split later
     // placeholder hash to record unqiue keys for our grams
-    var result = sentences.join("%%%");
+    var result = sentences.join("$$$");
     var copy = result.slice(0);
     var placeholders = {};
 
@@ -141,7 +70,23 @@ function gameDetector(grams, sentences) {
     };
 
     //delete those $ bills yo
-    return copy.split("%%%");
+    return copy.split("$$$");
+  }
+
+// helper methods
+
+// function that hashes our ngrams to our gameIDs to improve efficiency of lookup cost
+  function ngramToGameID (grams) {
+    let gameID = {};
+    for (let key in grams) {
+      grams[key].forEach( (gram) => gameID[gram] = key);
+    }
+    return gameID;
+  }
+
+  //function to convert our ngram and gameID to proper notation
+  function taggify (gameID, ngram) {
+    return `TAG{${gameID},${ngram}}`;
   }
 
   // simply a function to instert taggified ngram in the correct location
@@ -165,18 +110,4 @@ function gameDetector(grams, sentences) {
 
   // boom O(n^2) ( assuming .replace() is O(n) )
 
-
-
-
-// Ok wow this solution is much more code but hey I basically traded space to improve time complexity.
-// The costliest operation is when I loop through all ngrams, and then loop through until I find each instance
-// of each ngram  where I have multiple operations costing O(n). Therefore, I have reduced the time complexity
-//from O(n^4) to O(n^3) while using slightly more space which is definitely a worthy tradeoff.
-// Another optimization would be to associate each gram with it's own unique character string so we
-//dont even have to loop through to find all instances of the same ngram and the respective position
-// in fact I believe this is possible using some hashing function to spit out a unique special character string
-// which would potentially bring the time complexity to O(n^2) in exchange for a little more space.
-
-
   module.exports.gameDetector = gameDetector;
-  module.exports.gameDetectorOriginal = gameDetectorOriginal;
--- CodingChallenges/gameDetectorTest.js
+++ CodingChallenges/gameDetectorTest.js
@@ -1,5 +1,5 @@
 const gameDetector = require('./gameDetector.js').gameDetector;
-const gameDetectorOriginal = require('./gameDetector.js').gameDetectorOriginal;
+
 // debugging log:
   // need to take care of punctuation
   // tried to check word by word instead of by character(to have less iterations)
@@ -9,144 +9,6 @@ const gameDetectorOriginal = require('./gameDetector.js').gameDetectorOriginal;
   // also checked for overlap issues THIS ONE WAS BIG!
 
 let grams, sentences, output;
-
-// solves original problem
-    grams =
-      {
-      "CallOfDutyWW2": ["Call of duty world war two", "COD WW2", "COD WWII","WW2COD"],
-      "Fortnite": ["Fortnite", "Fort Nite"],
-      "Destiny": ["Destiny", "original Destiny game"],
-      "Destiny2": ["Destiny 2", "the last Destiny game", "Destiny II"],
-      "WorldOfWarcraft": ["WoW the game", "world of warcraft"],
-      };
-
-    sentences =
-      ["I liked the last Destiny game, now I play Fortnite",
-       "Lol, no comment about that",
-       "I'm still playing world of warcraft since ww2"];
-
-    output = ["I liked TAG{Destiny2,the last Destiny game}, now I play TAG{Fortnite,Fortnite}",
-      "Lol, no comment about that",
-      "I'm still playing TAG{WorldOfWarcraft,world of warcraft} since ww2"];
-
-
-
-  let i = 0;
-    gameDetectorOriginal(grams, sentences).forEach( (sentence) =>
-      {
-        if (sentence !== output[i]) {
-          throw "First Solution Failed Original Test Case";
-        }
-        i += 1;
-      }
-    );
-
-    console.log("First Solution Passed Original Test Case");
-
-// solves aggressive punctuation issue
-// this test case made me realize I was not handling the case where an n-gram
-// could have multiple punctuation associated
-
-    grams =
-      {
-      "CallOfDutyWW2": ["Call of duty world war two", "COD WW2", "COD WWII","WW2COD"],
-      "Fortnite": ["Fortnite", "Fort Nite"],
-      "Destiny": ["Destiny", "original Destiny game"],
-      "Destiny2": ["Destiny 2", "the last Destiny game", "Destiny II"],
-      "WorldOfWarcraft": ["WoW the game", "world of warcraft"],
-      };
-
-    sentences =
-      ["I liked the last Destiny game!!!!, now I play Fortnite",
-       "Lol, no comment about that",
-       "I'm still playing world of warcraft since ww2???"];
-
-    output = ["I liked TAG{Destiny2,the last Destiny game}!!!!, now I play TAG{Fortnite,Fortnite}",
-      "Lol, no comment about that",
-      "I'm still playing TAG{WorldOfWarcraft,world of warcraft} since ww2???"];
-
-
-
-    i = 0;
-    gameDetectorOriginal(grams, sentences).forEach( (sentence) =>
-      {
-        if (sentence !== output[i]) {
-          throw "First Solution Failed Punctuation Test Case";
-        }
-        i += 1;
-      }
-    );
-
-    console.log("First Solution Passed Punctuation Test Case");
-
-// maybe you did not want to handle case insensitive issue, but added it anyways
-// I decided that I wanted the implementation to be case sensitive because LOL can refer to League Of Legends while lol
-// should not trigger this
-    grams =
-      {
-      "LeagueOfLegends": ["League of Legends", "League"],
-      "ClashRoyale": ["Clash Royale", "Clash"],
-      };
-
-    sentences =
-      ["I used to play a lot of League of Legends, but then I learned javaScript instead!",
-       "Lol, no comment about that",
-       "I do occasionally play Clash because it's on my phone and a much shorter game"];
-
-    output = ["I used to play a lot of TAG{LeagueOfLegends,League of Legends}, but then I learned javaScript instead!",
-      "Lol, no comment about that",
-      "I do occasionally play TAG{ClashRoyale,Clash} because it's on my phone and a much shorter game"];
-
-      i = 0;
-      gameDetectorOriginal(grams, sentences).forEach( (sentence) =>
-        {
-          if (sentence !== output[i]) {
-            throw "First Solution Failed Case Sensitive Test Case";
-          }
-          i += 1;
-        }
-      );
-
-      console.log("First Solution Passed Case Sensitive Test Case");
-
-// what if we have some overlapping of n-grams between different games? Something like this:
-// My implementation is designed to trigger on the first word of the n-gram so it will handle
-// this case just fine
-// Clash is also replaced and not checked again so we don't have
-// "I love playing original clash" => "I love playing TAG{ClashOfClans,original clash} TAG{ClashRoyale,clash}""
-
-      grams =
-        {
-        "LeagueOfLegends": ["League Of Legends", "League", "LOL"],
-        "ClashRoyale": ["Clash Royale", "Clash"],
-        "ClashOfClans": ["Clash Of Clans", "original Clash"]
-        };
-
-      sentences =
-        ["I used to play a lot of League, but then I learned javaScript instead!",
-         "I love playing original Clash",
-         "I love playing Clash",
-         "I do occasionally play Clash because it's on my phone and a much shorter game"];
-
-      output = ["I used to play a lot of TAG{LeagueOfLegends,League}, but then I learned javaScript instead!",
-        "I love playing TAG{ClashOfClans,original Clash}",
-        "I love playing TAG{ClashRoyale,Clash}",
-        "I do occasionally play TAG{ClashRoyale,Clash} because it's on my phone and a much shorter game"];
-
-        i = 0;
-        gameDetectorOriginal(grams, sentences).forEach( (sentence) =>
-          {
-            if (sentence !== output[i]) {
-              throw "First Solution Failed Overlap Test Case";
-            }
-            i += 1;
-          }
-        );
-
-        console.log("First Solution Passed Overlap Test Case");
-
-// multiple instances of the same ngram are changed
-
 
 // testing new "improved" code
 grams =
@@ -169,17 +31,17 @@ output = ["I liked TAG{Destiny2,the last Destiny game}, now I play TAG{Fortnite,
 
 
 
-i = 0;
+let i = 0;
 gameDetector(grams, sentences).forEach( (sentence) =>
   {
     if (sentence !== output[i]) {
-      throw "Second Solution Failed Original Test Case";
+      throw "Solution Failed Original Test Case";
     }
     i += 1;
   }
 );
 
-console.log("Second Solution Passed Original Test Case");
+console.log("Solution Passed Original Test Case");
 
 // solves aggressive punctuation issue
 // this test case made me realize I was not handling the case where an n-gram
@@ -209,13 +71,13 @@ i = 0;
 gameDetector(grams, sentences).forEach( (sentence) =>
   {
     if (sentence !== output[i]) {
-      throw "Second Solution Failed Punctuation Test Case";
+      throw "Solution Failed Punctuation Test Case";
     }
     i += 1;
   }
 );
 
-console.log("Second Solution Passed Punctuation Test Case");
+console.log("Solution Passed Punctuation Test Case");
 
 // maybe you did not want to handle case insensitive issue, but added it anyways
 // I decided that I wanted the implementation to be case sensitive because LOL can refer to League Of Legends while lol
@@ -239,13 +101,13 @@ output = ["I used to play a lot of TAG{LeagueOfLegends,League of Legends}, but t
   gameDetector(grams, sentences).forEach( (sentence) =>
     {
       if (sentence !== output[i]) {
-        throw "Second Solution Failed Case Sensitive Test Case";
+        throw "Solution Failed Case Sensitive Test Case";
       }
       i += 1;
     }
   );
 
-  console.log("Second Solution Passed Case Sensitive Test Case");
+  console.log("Solution Passed Case Sensitive Test Case");
 
 // what if we have some overlapping of n-grams between different games? Something like this:
 // My implementation is designed to trigger on the first word of the n-gram so it will handle
@@ -256,18 +118,18 @@ output = ["I used to play a lot of TAG{LeagueOfLegends,League of Legends}, but t
 
   grams =
     {
-    "LeagueOfLegends": ["League Of Legends", "League"],
+    "LeagueOfLegends": ["League of Legends", "League"],
     "ClashRoyale": ["Clash Royale", "Clash"],
     "ClashOfClans": ["Clash Of Clans", "original Clash"]
     };
 
   sentences =
-    ["I used to play a lot of League Of Legends, but then I learned javaScript instead!",
+    ["I used to play a lot of League of Legends, but then I learned javaScript instead!",
      "I love playing original Clash",
      "I love playing Clash",
      "I do occasionally play Clash because it's on my phone and a much shorter game"];
 
-  output = ["I used to play a lot of TAG{LeagueOfLegends,League Of Legends}, but then I learned javaScript instead!",
+  output = ["I used to play a lot of TAG{LeagueOfLegends,League of Legends}, but then I learned javaScript instead!",
     "I love playing TAG{ClashOfClans,original Clash}",
     "I love playing TAG{ClashRoyale,Clash}",
     "I do occasionally play TAG{ClashRoyale,Clash} because it's on my phone and a much shorter game"];
@@ -276,10 +138,47 @@ output = ["I used to play a lot of TAG{LeagueOfLegends,League of Legends}, but t
     gameDetector(grams, sentences).forEach( (sentence) =>
       {
         if (sentence !== output[i]) {
-          throw "Second Solution Failed Overlap Test Case";
+          throw "Solution Failed Overlap Test Case";
         }
         i += 1;
       }
     );
 
-    console.log("Second Solution Passed Overlap Test Case");
+    console.log("Solution Passed Overlap Test Case");
+
+// multiple instances of the same ngram?
+  grams =
+    {
+    "LeagueOfLegends": ["League of Legends", "League"],
+    "ClashRoyale": ["Clash Royale", "Clash"],
+    "ClashOfClans": ["Clash Of Clans", "original Clash"],
+    "Fifa18": ["Fifa","latest Fifa"]
+    };
+
+  sentences =
+    ["I used to play a lot of League of Legends, but then I learned javaScript instead!",
+     "I love playing original Clash",
+     "I love playing Clash",
+     "Clash is cool but Fifa is cooler!",
+     "I do occasionally play Clash because it's on my phone and a much shorter game",
+     "There is no way you beat me at the latest Fifa"];
+
+  output = ["I used to play a lot of TAG{LeagueOfLegends,League of Legends}, but then I learned javaScript instead!",
+    "I love playing TAG{ClashOfClans,original Clash}",
+    "I love playing TAG{ClashRoyale,Clash}",
+    "TAG{ClashRoyale,Clash} is cool but TAG{Fifa18,Fifa} is cooler!",
+    "I do occasionally play TAG{ClashRoyale,Clash} because it's on my phone and a much shorter game",
+    "There is no way you beat me at the TAG{Fifa18,latest Fifa}"
+  ];
+
+    i = 0;
+    gameDetector(grams, sentences).forEach( (sentence) =>
+      {
+        if (sentence !== output[i]) {
+          throw "Solution Failed Repeated Ngram Test Case";
+        }
+        i += 1;
+      }
+    );
+
+    console.log("Solution Passed Repeated Ngram Test Case");
