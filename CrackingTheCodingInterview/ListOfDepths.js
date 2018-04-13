509957a66795235f9f4c3bf2df4ca7fd2eed12b7
Arpan NLN <arpannln@ucla.edu>
2018-04-13 16:04:59 -0700

    dependencies

--- CrackingTheCodingInterview/ListOfDepths.js
+++ CrackingTheCodingInterview/ListOfDepths.js
@@ -5,6 +5,11 @@
 //    4  5  6   7
 //linked lists -> 1, 2 -> 3, 4 -> 5 -> 6 -> 7
 
+
+// BST
+//      4
+//   2    6
+// 1  3  5  7
 class Node {
   constructor (value, left, right) {
     this.value = value;
@@ -13,15 +18,16 @@ class Node {
   }
 }
 
-var node4 = new Node(4, null, null);
+var node1 = new Node(1, null, null);
+var node3 = new Node(3, null, null);
 var node5 = new Node(5, null, null);
-var node6 = new Node(6, null, null);
 var node7 = new Node(7, null, null);
 
-var node2 = new Node(2, node4, node5);
-var node3 = new Node(3, node6, node7);
+var node2 = new Node(2, node1, node3);
+var node6 = new Node(6, node5, node7);
 
-var node1 = new Node(1, node2, node3);
+var node4 = new Node(4, node2, node6);
+
 
 
 function createLists(head) {
@@ -54,3 +60,5 @@ function isValidBST(head) {
 
   return true;
 }
+
+console.log(isValidBST(node1));
---
+++ CrackingTheCodingInterview/dependencies.js
@@ -0,0 +1,3 @@
+//projects: a, b, c, d, e, f
+//dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
+//output: f, e, a, b, d, c
