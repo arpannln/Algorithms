a40652358ef2b9b81a4fc556b68e461e5f475be5
Arpan NLN <arpannln@ucla.edu>
2018-04-16 22:20:39 -0700

    subsetstoo

--- CrackingTheCodingInterview/permutations.js
+++ CrackingTheCodingInterview/permutations.js
@@ -1,7 +1,7 @@
 //[1, 2, 3] => [1, 2, 3], [1, 3, 2], [2, 3, 1], [2, 1, 3], [3, 1, 2], [3, 2, 1]
 
  const permutator = (inputArr) => {
-   let result = [];
+   var result = [];
    //define recursive function here
    const permute = (arr, m=[]) => {
      //nothing left to permute
@@ -24,3 +24,31 @@
  };
 
  console.log(permutator([1,2,3]));
+
+//[1, 2, 3] => [1, 2, 3], [1, 3], [1, 2], [2, 3], [1], [2], [3], []
+
+const subsetter = (inputArr) => {
+  var result = [];
+
+  const subset = (arr) => {
+    if (arr.length === 0) {
+      result.push([]);
+    } else {
+        if (!result.includes(arr)) {
+            result.push(arr);
+        }
+        for (let i = 0; i < arr.length; i++) {
+          var curr = arr.slice();
+          curr.splice(i, 1);
+
+          subset(curr.slice());
+        }
+    }
+  };
+
+  subset(inputArr);
+
+  return result;
+};
+
+console.log(subsetter([1, 2, 3]));
