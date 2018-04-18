6de3b73f4a83e2a52668ff902d5b0eab6b2d2147
Arpan NLN <arpannln@ucla.edu>
2018-04-18 13:34:15 -0700

    reversi

---
+++ CrackingTheCodingInterview/genericLinkedList.js
@@ -0,0 +1,6 @@
+export default class Link {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
---
+++ CrackingTheCodingInterview/reverse.js
@@ -0,0 +1,10 @@
+import Link from './genericLinkedList.js';
+
+
+
+var link6 = new Link(1, null);
+var link5 = new Link(1, link6);
+var link4 = new Link(1, link5);
+var link3 = new Link(1, link4);
+var link2 = new Link(1, link3);
+var link1 = new Link(1, link2);
