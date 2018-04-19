7f980115aa6b02e6b6997ebcbdfe80cf5951e188
Arpan NLN <arpannln@ucla.edu>
2018-04-19 14:08:30 -0700

    more test cases

--- CrackingTheCodingInterview/firstCommon.js
+++ CrackingTheCodingInterview/firstCommon.js
@@ -21,6 +21,18 @@ var node4 = new Node(4, node2, node6);
 //      4
 //   2    6
 // 1  3  5  7
+//            8
+
+
+var node8 = new Node(1, null, null);
+var node9 = new Node(3, null, null);
+var node10 = new Node(5, null, null);
+var node11 = new Node(7, null, null);
+
+var node12 = new Node(2, node1, node3);
+var node13 = new Node(6, node5, node7);
+
+var node14 = new Node(4, node2, node6);
 
 const findCommonAncestor = (head, A, B) => {
   //one approach I could put all children of every element in the hash and then
@@ -29,4 +41,7 @@ const findCommonAncestor = (head, A, B) => {
 
   // if it is a BST
   // do in order traversal pick first node that falls between the two
+  // but hey its not a BST
+
+
 };
