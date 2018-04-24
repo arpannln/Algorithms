b6f5dc2a146db64025b68a73894c3b0fbb99f153
Arpan NLN <arpannln@ucla.edu>
2018-04-24 11:31:48 -0700

    if i was asked this I might actually prematurely...

---
+++ leetcode/containsDup.js
@@ -0,0 +1,12 @@
+//honestly easiest question ever
+
+var containsDuplicate = function(nums) {
+    let seen = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (seen[nums[i]]) return true;
+        seen[nums[i]] = true;
+    }
+
+    return false;
+};
