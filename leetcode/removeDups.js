6ff523eda46788b36c8845c549582ea7a6b8a38c
Arpan NLN <arpannln@ucla.edu>
2018-04-23 18:17:16 -0700

    removed Dups baby

---
+++ leetcode/removeDups.js
@@ -0,0 +1,16 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    while (i < nums.length - 1) {
+        if (nums[i] === nums[i+1]) {
+            nums.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    console.log(nums);
+    return;
+};
