397e0cf9d41db283253cce98717abd1eadad8248
Arpan NLN <arpannln@ucla.edu>
2018-05-03 22:25:55 -0700

    make aite change

--- leetcode/makeBetterChange.js
+++ leetcode/makeBetterChange.js
@@ -1,3 +1,35 @@
-// [5, 8, 12, 17] 30
+// [1, 5, 8, 12, 17] 30
 
-// 
+// start from the end
+
+const makeBetterChange = (inputArr, target) => {
+  let result = [[]];
+
+  const makeChange = (arr, targ, accum = []) => {
+    console.log(arr, targ, accum);
+    if (targ === 0) {
+      result.push(accum);
+      return;
+    }
+    if (arr.length === 0) {
+      return;
+    }
+    if (targ < 0) {
+      return;
+    }
+    //we want to fork into 2 here, one where we include the last element
+    if (targ - arr[arr.length - 1] >= 0) {
+      makeChange(arr.slice(), targ - arr[arr.length - 1], accum.concat([arr[arr.length - 1]]) );
+    } else {
+      makeChange(arr.slice(0, arr.length - 1), targ, accum);
+    }
+    //one where we dont
+
+  };
+
+  makeChange(inputArr, target);
+
+  return result.sort()[0];
+};
+
+console.log(makeBetterChange([1, 5, 8, 12, 17], 30));
