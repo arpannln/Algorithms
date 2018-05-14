cf66c1ab79af010ef78cb96c03a8009f377b6646
Arpan NLN <arpannln@ucla.edu>
2018-05-14 13:33:37 -0700

    initial approach

--- leetcode/findDupNumber.js
+++ leetcode/findDupNumber.js
@@ -8,10 +8,39 @@
 // Credits:
 // Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 // nlogn approach?
+//examples [1, 2, 3, 3], [1, 2, 2, 3]
+
+//initial tackle
+var findDuplicate = function(nums) {
+    //total sum has to be like median * 2n
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+    let totalExpectedSum;
+
+    if (nums.length % 2 === 0) {
+        totalExpectedSum = (max * (max + 1))/2;
+    } else {
+        totalExpectedSum = (max * max)/2;
+    }
+
+    console.log(totalExpectedSum);
+
+    let actualSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        actualSum += nums[i];
+    }
+
+    let overflow = nums.length + 1 - max;
+
+    let result = (actualSum - totalExpectedSum) / overflow;
+
+    return result;
+};
 const findDup = (inputArr) => {
   //sum up all the elements
   let sum = 0;
-  let expectedSum = 
+
   for (let i = 0; i < inputArr.length; i++) {
 
   }
