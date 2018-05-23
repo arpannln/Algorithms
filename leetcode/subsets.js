36827ccfc230a5ba8e0e131350936e44dfdfbc9a
Arpan NLN <arpannln@ucla.edu>
2018-05-23 16:56:45 -0700

    slicker version of subsets

--- leetcode/subsets.js
+++ leetcode/subsets.js
@@ -1,22 +1,39 @@
+// const subsetter = (inputArr) => {
+//
+//   let result = [];
+//   //for every element in the array, make a decision to include it or not;
+//   const subset = (arr, i) => {
+//     if (i === arr.length) {
+//       result.push(arr);
+//     } else {
+//       let copy = arr.slice();
+//       subset(copy.slice(), i + 1);
+//       copy.splice(i, 1);
+//       subset(copy.slice(), i);
+//     }
+//   };
+//
+//   subset(inputArr, 0);
+//
+//   return result;
+// };
+
 const subsetter = (inputArr) => {
 
   let result = [];
   //for every element in the array, make a decision to include it or not;
-  const subset = (arr, i) => {
-    if (i === arr.length) {
-      result.push(arr);
+  const subset = (arr, accum = []) => {
+    if (arr.length === 0) {
+      result.push(accum);
     } else {
-      let copy = arr.slice();
-      subset(copy.slice(), i + 1);
-      copy.splice(i, 1);
-      subset(copy.slice(), i);
+      subset(arr.slice(1), accum.concat([arr[0]]));
+      subset(arr.slice(1), accum);
     }
   };
 
-  subset(inputArr, 0);
+  subset(inputArr);
 
   return result;
 };
-
 
 console.log(subsetter([1, 2, 3]));
