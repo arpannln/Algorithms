764f33dfef6aa6c2d00cec70507c59fe99b3b1f6
Arpan NLN <arpannln@ucla.edu>
2018-06-04 20:26:17 -0700

    not again

--- MethodChaining/jquerylite.js
+++ MethodChaining/jquerylite.js
@@ -1,3 +1,22 @@
-const $ = () => {
-  
+let options = { all: true };
+
+const $ = function(el, option) {
+  let elements = option.all ? document.querySelectorAll(el) : document.querySelector(el);
+  return new JQueryObj(Array.from(elements));
 };
+
+// I think we need another object
+class JQueryObj {
+  
+  constructor(obj) {
+    this.domElements = obj;
+  }
+
+  nthChild(n) {
+    return this.domElements[n];
+  }
+
+}
+
+
+console.log($('p', options).nthChild(1));
