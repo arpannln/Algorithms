ff6e7fcbd94d63721f9413d2069234b20f6ac9e7
Arpan NLN <arpannln@ucla.edu>
2018-06-28 12:22:28 -0700

    possible plan

--- leetcode/FindingLeft.js
+++ leetcode/FindingLeft.js
@@ -1,1 +1,6 @@
 //Given a binary tree, find the leftmost value in the last row of the tree.
+//thinking we could do breadth first and keep putting left most children in an array
+//return the last one that shows up? 
+var findBottomLeftValue = function(root) {
+
+};
