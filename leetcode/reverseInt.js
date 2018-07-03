e51e86d50949780adcac785a4de421091cd06d18
Arpan NLN <arpannln@ucla.edu>
2018-07-03 09:43:59 -0700

    reverse int

---
+++ leetcode/reverseInt.js
@@ -0,0 +1,19 @@
+var reverse = function(x) {
+    let result = [];
+    let negative = 1;
+
+    if (x < 0) {
+        x = x * -1;
+        negative = -1;
+    }
+
+    let strung = String(x).split('');
+    strung.reverse();
+    let reversedNum = parseInt(strung.join(''));
+
+    if (reversedNum > Math.pow(2, 31)) {
+        return 0;
+    } else {
+        return reversedNum * negative;
+    }
+};
